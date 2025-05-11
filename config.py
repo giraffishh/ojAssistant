26d9d44d4dc1c5e511fdfe249d705a32d02aa4aa
giraffishh <938793658@qq.com>
2025-05-11 21:33:02 +0800

    feat：支持一次提交多个java文件，适配junit类题目

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """一些配置与设置"""
-USERNAME = "REMOVED"
-PASSWORD = "REMOVED"
+USERNAME = "12412832"
+PASSWORD = "Ymxnkd*2673"
 COOKIES_FILE = 'oj_cookies.pkl'
-WORK_DIRECTORY = ""  # 你的Java作业所在的目录
-AUTO_SELECT_COURSE = False
+WORK_DIRECTORY = r"C:\Users\93879\Documents\JavaPractice"  # 你的Java作业所在的目录
+AUTO_SELECT_COURSE = True
 AUTO_SELECT_HOMEWORK = True
 MAX_RECORDS_TO_SHOW = 3
